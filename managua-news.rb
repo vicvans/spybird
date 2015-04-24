6223f1db7d1d29cc15d8b3c91db02627b59f70c4
vicvans <vicvans20@gmail.com>
2015-04-24 13:47:30 -0600

    Add home page and dinamic term tracker

--- managua-news.rb
+++ managua-news.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
-#TUTORIAL
 require 'rubygems'
 require 'oauth'
 require 'json'
 require 'tweetstream'
-#require 'debugger'
-require 'multi_json'
 #Avoid conflict between tweetstream and sinatra
 set :server, 'webrick'
+#js read sinatra parameters(vars)
+require 'gon-sinatra'
+
+Sinatra::register Gon::Sinatra
 
 TweetStream.configure do |config|
 	config.consumer_key = 'ZgWljSLf5Ph7XdFZmNwHDpOc2'
@@ -16,38 +17,59 @@ TweetStream.configure do |config|
 	config.oauth_token_secret = 'CnWW3I8TMhbrYirqiFCtCrXh4ipmwMYppR3YYIJG6ibxj'
 	config.auth_method = :oauth
 end
-# Probando
-def render_tweet
+# Extract and convert tweet from api to json
+def render_tweet(term)
 	result=[];
-	TweetStream::Client.new.track('like') do |status,client|
+	TweetStream::Client.new.track(term) do |status,client|
 		result << status
-		puts status
 		client.stop if result.size>=1;
 	end
 	result[0].attrs.to_json
 end
 
-def render_tweet2
-	this_h = {:nombre => "VictorVanegas", :alias => "V.V.", :love => "Kathy"}
-	puts this_h
-	this_j = this_h.to_json
-end
-#------------
-
 get '/' do
-  	"Hello world, it's #{Time.now} at the server!"
+  	erb :index
 end
 
+#Main functionality open streaming with the desired term
+get '/track' do
+#	Store term to read from JS to stream
+	@this_var = params['term']
+	gon.this_var = @this_var
+	erb :managua_tweets2, :locals => {:term => params['term']}
+end
+
+get '/testo' do
+	@this_var = params['term']
+	puts "---------------------"
+	puts @this_var
+	puts @this_var.class
+	@this_var.nil?
+end
+
+
+#AJAX Call to json extraction
+get '/render_tweets.json/:term' do
+	content_type :json
+	render_tweet(params[:term])
+end
+
+not_found do
+	"Hey watt'cha doing here?"
+end
+#------------------------------------------------------
+# Not working ways----------------------
+#Standar way
 get '/managua' do
 	# code = "<%=@statuses[0].text %>"
 	erb :managua_tweets
 end
-
-get '/managua2' do
-	#erb :managua_tweets2, :locals => {:par => params[:par]}
-	erb :managua_tweets2
+#Daemonazing
+get '/managuad' do
+	erb :managua_tweets_d
 end
-
+#----------------------
+#Stream
 get '/stream.rb' do
 	statuses =[]
 	TweetStream::Client.new.track("like") do |status,client|
@@ -59,21 +81,13 @@ get '/stream.rb' do
 	puts statuses
 	puts statuses.class
 	puts statuses[0].class
-
 end
-
-get '/render_tweets.json' do
-	content_type :json
-	render_tweet
+#---------------------------------
+#Test of jsoon
+def render_tweet2
+	this_h = {:nombre => "VictorVanegas", :alias => "V.V.", :love => "Kathy"}
+	puts this_h
+	this_j = this_h.to_json
 end
+#------------
 
-
-get '/managuad' do
-	erb :managua_tweets_d
-end
-
-
-
-not_found do
-	"Hey watt'cha doing here?"
-end
--- public/js/stream.js
+++ public/js/stream.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
-	
 	function streamUpdate(){
 			console.log("Time!");
 			$.ajax({
-				url: "/render_tweets.json",
+				url: "/render_tweets.json/"+gon.this_var,
 				dataType: 'json',
 				success:function(response){
 					console.log(response);
 					$tweet = response.text;
 					$username = response.user.screen_name;
-					$("table").append("<tr><td>"+$username+"</td><td>"+$tweet+"</td></tr>");
+					$("table").append("<tr><td>["+$username+"]</td><td>"+$tweet+"</td></tr>");
 				}
 			});
 	}
-	var tid = setInterval(streamUpdate,9000);
-
+	var tid = setInterval(streamUpdate,6000);
+	$("#btn-stop").click(function(){
+		abortStream();
+		alert("Stream stopped!");
+	});
 	function abortStream(){
 		clearInterval(tid);
 	}
