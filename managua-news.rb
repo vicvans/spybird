58c78dfd45fd9aba0edc89888bdbf6e15f726f08
vicvans20 <vicvans20@gmail.com>
2016-11-30 12:39:50 -0600

    Renew app. - Upgrade to Bootstrap 4 Alpha 5. - jquery 3.0.0 - Added a default layout. - Remove unused routes.

--- managua-news.rb
+++ managua-news.rb
@@ -22,6 +22,7 @@ def render_tweet(term)
 	result=[];
 	begin
 		TweetStream::Client.new.track(term) do |status,client|
+			puts status
 			result << status
 			client.stop if result.size>=1;
 		end
@@ -41,14 +42,11 @@ def render_tweet(term)
 end
 
 get '/' do
-  	erb :index
+	erb :index
 end
 
 #Main functionality open streaming with the desired term
 get '/track' do
-#	Store term to read from JS to stream
-	@this_var = params['term']
-	gon.this_var = @this_var
 	erb :managua_tweets2, :locals => {:term => params['term']}
 end
 
@@ -62,49 +60,13 @@ end
 
 
 #AJAX Call to json extraction
-get '/render_tweets.json/:term' do
+get '/render_tweets.json' do
+	puts "-----------------"
+	puts params
 	content_type :json
-	render_tweet(params[:term])
+	return render_tweet(params[:term])
 end
 
 not_found do
 	"Hey watt'cha doing here?"
 end
-
-get '/secret/nyawnya' do
-	erb :secureto
-end
-#------------------------------------------------------
-# Not working ways----------------------
-#Standar way
-get '/managua' do
-	# code = "<%=@statuses[0].text %>"
-	erb :managua_tweets
-end
-#Daemonazing
-get '/managuad' do
-	erb :managua_tweets_d
-end
-#----------------------
-#Stream
-get '/stream.rb' do
-	statuses =[]
-	TweetStream::Client.new.track("like") do |status,client|
-		statuses << status
-		puts "#{status.text}"
-		client.stop if statuses.size >=5
-	end
-	puts '------------------------------------------'
-	puts statuses
-	puts statuses.class
-	puts statuses[0].class
-end
-#---------------------------------
-#Test of jsoon
-def render_tweet2
-	this_h = {:nombre => "VictorVanegas", :alias => "V.V.", :love => "Kathy"}
-	puts this_h
-	this_j = this_h.to_json
-end
-#------------
-
